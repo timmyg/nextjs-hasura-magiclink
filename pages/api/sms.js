34887857691e78b9cc0ffe2a7978fb264227f57b
tim <timmyg13@gmail.com>
2020-06-19 12:51:32 -0400

    create

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -4,5 +4,25 @@ export default async (req, res) => {
     const twiml = new MessagingResponse();
     console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
     twiml.message('The Robots are coming! Head for the hills!');
+    const babyId = 1;
+
+    fetch('/api/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(
+          {
+            "query":"mutation insert_single_activity($object: activities_insert_input! ) {insert_activities_one(object: $object) { id type baby_id created_at updated_at start_at }}",
+            "variables": {
+              "object": {
+                text: req.body.Body,
+                baby_id: babyId
+              }
+            },
+            "operationName":"insert_single_activity"}
+        ),
+      })
+
     return res.status(200).json({message: "ok"})
 }
