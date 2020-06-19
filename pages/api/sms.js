063550b19bfe340d5b6adee0ad7aef33744ad2ef
tim <timmyg13@gmail.com>
2020-06-19 12:44:22 -0400

    tweak

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -4,5 +4,5 @@ export default async (req, res) => {
     const twiml = new MessagingResponse();
     console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
     twiml.message('The Robots are coming! Head for the hills!');
-    return res.status(200).json(data)
+    return res.status(200).json({message: "ok"})
 }
