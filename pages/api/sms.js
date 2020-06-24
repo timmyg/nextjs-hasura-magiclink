208df4d4afc4839c718f1e8c4c60a430895cf6a9
tim <timmyg13@gmail.com>
2020-06-24 17:37:28 -0400

    logging

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -2,9 +2,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 export default async (req, res) => {
     const twiml = new MessagingResponse();
-    twiml.message('The Robots are coming! Head for the hills!');
     const babyId = 1;
-    // const baseUrl = `https://${req.headers.host}`;
     console.log(process.env.GRAPHQL_ENDPOINT, process.env.HASURA_GRAPHQL_ADMIN_SECRET);
     console.log(req.body.Body, babyId);
 
@@ -19,7 +17,7 @@ export default async (req, res) => {
             "query":"mutation insert_single_activity($object: activities_insert_input! ) {insert_activities_one(object: $object) { id type baby_id created_at updated_at start_at }}",
             "variables": {
               "object": {
-                text: req.body.Body,
+                text: req.body.Body.trim(),
                 baby_id: babyId
               }
             },
@@ -27,7 +25,7 @@ export default async (req, res) => {
         ),
       })
     const data = await gqlResponse.json()
-    console.log({data});
+    console.log({data: JSON.stringify(data)});
 
     return res.status(200).json({message: "ok"})
 }
