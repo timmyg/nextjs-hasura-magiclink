9a43a5ba8ce9c27f9f84fc8ce80ece1dd8051d25
tim <timmyg13@gmail.com>
2020-06-24 17:52:56 -0400

    fixes

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -4,11 +4,8 @@ export default async (req, res) => {
     const twiml = new MessagingResponse();
     twiml.message('The Robots are coming! Head for the hills!');
     const babyId = 1;
-    // const baseUrl = `https://${req.headers.host}`;
-    console.log(process.env.GRAPHQL_ENDPOINT, process.env.HASURA_GRAPHQL_ADMIN_SECRET);
-    console.log(req.body.Body, babyId);
 
-    const gqlResponse = await fetch(process.env.GRAPHQL_ENDPOINT, {
+    await fetch(process.env.GRAPHQL_ENDPOINT, {
         method: 'POST',
         headers: {
           'Content-type': 'application/json',
@@ -19,15 +16,13 @@ export default async (req, res) => {
             "query":"mutation insert_single_activity($object: activities_insert_input! ) {insert_activities_one(object: $object) { id type baby_id created_at updated_at start_at }}",
             "variables": {
               "object": {
-                text: req.body.Body.trim(),
+                text: req.body.Body,
                 baby_id: babyId
               }
             },
             "operationName":"insert_single_activity"}
         ),
       })
-    const data = await gqlResponse.json()
-    console.log({data: JSON.stringify(data)});
 
     return res.status(200).json({message: "ok"})
 }
